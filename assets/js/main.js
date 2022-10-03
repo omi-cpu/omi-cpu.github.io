b83024d7927b62e64eb34a59cd13b4bda6ed03e9
omiete <user@users-MacBook-Pro.local>
2022-10-03 14:11:41 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,19 +230,7 @@
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  
 
   /**
    * Initiate Pure Counter 
